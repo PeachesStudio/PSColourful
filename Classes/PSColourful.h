9cfa118eff2823ad0fd66de9556a1c58796e1b37
liuqin.sheng <sliuqin@gmail.com>
2015-04-26 16:42:10 +0800

    update

--- Classes/PSColourful.h
+++ Classes/PSColourful.h
@@ -2,7 +2,7 @@
 // Created by liuqin.sheng on 9/2/14.
 // Copyright (c) 2014 Peaches Studio. All rights reserved.
 //
-
+#import <Foundation/Foundation.h>
 #import "PSHexColor.h"
 
 @interface PSColourful : NSObject
--- Classes/PSHexColor.h
+++ Classes/PSHexColor.h
@@ -3,7 +3,7 @@
 // Copyright (c) 2014 Peaches Studio. All rights reserved.
 //
 
-
+#import <Foundation/Foundation.h>
 #define RGBCOLOR(r, g, b)     [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:1.0]
 #define RGBACOLOR(r, g, b, a)  [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:(a)]
 #define HEXRGBCOLOR(h)      RGBCOLOR(((h>>16)&0xFF), ((h>>8)&0xFF), (h&0xFF))
